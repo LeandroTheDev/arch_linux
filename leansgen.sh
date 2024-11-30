5c68a71944984771fc8163a14a2875a941568bc4
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2024-11-29 21:25:29 -0300

    removed cache from leansgen.sh

--- leansgen.sh
+++ leansgen.sh
@@ -32,21 +32,21 @@ fi
 export INSTALLPARTITION=$disk
 
 # Device partitioning
-sh -c "$(curl -sS -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/LeandroTheDev/arch_linux/refs/heads/leansgen/Installation/disk_creation.sh)"
+sh -c "$(curl -sS https://raw.githubusercontent.com/LeandroTheDev/arch_linux/refs/heads/leansgen/Installation/disk_creation.sh)"
 if [ $? -ne 0 ]; then
     echo "The disk creation script has failed."
     exit 1
 fi
 
 # Installing Linux
-sh -c "$(curl -sS -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/LeandroTheDev/arch_linux/refs/heads/leansgen/Installation/linux_install.sh)"
+sh -c "$(curl -sS https://raw.githubusercontent.com/LeandroTheDev/arch_linux/refs/heads/leansgen/Installation/linux_install.sh)"
 if [ $? -ne 0 ]; then
     echo "The Linux installation has failed."
     exit 1
 fi
 
 # Configuring device and boot
-arch-chroot /mnt bash -c 'export INSTALLPARTITION="$INSTALLPARTITION" && sh -c "$(curl -sS -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/LeandroTheDev/arch_linux/refs/heads/leansgen/Installation/device_config.sh)"'
+arch-chroot /mnt bash -c 'export INSTALLPARTITION="$INSTALLPARTITION" && sh -c "$(curl -sS https://raw.githubusercontent.com/LeandroTheDev/arch_linux/refs/heads/leansgen/Installation/device_config.sh)"'
 if [ $? -ne 0 ]; then
     echo "The device installation has failed."
     exit 1
