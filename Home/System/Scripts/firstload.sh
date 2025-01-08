ab681bcddd52c152ef7ab83c380676fbacf2d14a
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2025-01-08 16:58:54 -0300

    home plasma configurations

--- Home/System/Scripts/firstload.sh
+++ Home/System/Scripts/firstload.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# Enable sound services
 systemctl --user enable wireplumber.service pipewire.service pipewire-pulse.service
+
+# Plasma Configurations
+sed -i "s/{USERNAME}/$username/g" "/home/$username/.config/kscreenlockerrc"
+sed -i "s/{USERNAME}/$username/g" "/home/$username/.config/plasma-org.kde.plasma.desktop-appletsrc"
 
 # This script will auto delete after executing
 rm -rf $HOME/System/Scripts/firstload.sh # Deleting the script
