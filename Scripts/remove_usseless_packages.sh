0d8732fa5236e9caded12b09100b8cfa6b5cce0f
Lean's <106118473+LeandroTheDev@users.noreply.github.com>
2025-05-17 14:27:37 -0300

    Update remove_usseless_packages.sh

--- Scripts/remove_usseless_packages.sh
+++ Scripts/remove_usseless_packages.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-# Get all packages that has been instaled without your consense
-dependencies_packages=$(pacman -Qdq)
+if [[ $EUID -ne 0 ]]; then
+  echo "No privilegies..."
+  exit 1
+fi
 
-# Swiping all packages
-for package in $dependencies_packages; do
-    if ! sudo pacman -Rns --noconfirm "$package"; then
-        echo "$package. ignoring..."
-    fi
+while true; do
+  # Get all packages that has been instaled without your consense
+  dependencies_packages=$(pacman -Qdq)
+
+  removed=false
+
+  # Swiping all packages
+  for package in $dependencies_packages; do
+      if ! pacman -Rns --noconfirm "$package"; then
+          echo "$package. ignoring..."
+      else
+          removed=true
+      fi
+  done
+
+  if [[ "$removed" == false ]]; then
+    echo "No packages was removed, your pacman dependency is clear"
+    break
+  fi
 done
