f9479c35136212b2390cc35614c592d19cd7fbfe
Lean's <106118473+LeandroTheDev@users.noreply.github.com>
2025-12-24 07:58:14 -0300

    Enhance update_and_reboot.sh with comments and typo fix
    Added comments to explain the script's purpose and fixed a typo in the success message.

---
+++ Scripts/update_and_reboot.sh
@@ -0,0 +1,24 @@
+# This script will update your system remove all cache
+# clean journals
+# remove previous versions from package
+# and finally reboot
+
+#!/bin/sh
+updates=$(pacman -Qu | awk '{print $1}')
+if [ -z "$updates" ]; then
+    echo "No available updates, reboot cancelled"
+    exit 0
+fi
+
+sudo pacman -Syu --noconfirm
+if [ $? -ne 0 ]; then
+    echo "Failed to update the system."
+    exit 1
+fi
+
+sudo pacman -Scc --noconfirm
+sudo paccache -ruk0
+sudo journalctl --vacuum-size=200M
+
+echo "Update success, rebotting..."
+systemctl reboot
